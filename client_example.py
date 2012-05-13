5a5628fb4f729cb3a6ae42594c9f2dbbcc644591
Igor Popov <ihptru@gmail.com>
2012-05-13 15:46:06 +0400

    detect minor changes in games

--- client_example.py
+++ client_example.py
@@ -4,9 +4,10 @@ import socket
 
 network = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
 
-network.connect(("localhost",  33321))
-while True:
-    data = network.recv(1024)
-    print (data.decode())
-    
-network.close()
+network.connect(("localhost",  33322))
+try:
+    while True:
+        data = network.recv(1024)
+        print (data.decode())
+except:
+    network.close()
--- stream.py
+++ stream.py
@@ -15,7 +15,7 @@ class Pinger:
     def pinger(self):
         url = 'http://master.open-ra.org/list_json.php'
         while self.stop_thread == [0]:
-            time.sleep(8)
+            time.sleep(5)
             self.lock.pop(0)
             self.lock.append(1)
             try:
@@ -36,45 +36,87 @@ class Client:
     def __init__(self, pinger_data, lock):
         self.games = pinger_data
         self.lock = lock
+        self.stop_thread = [0]
 
     def work_with_client(self, sock, addr):
         state = self.games[:]
-        sock.send(str(state).encode())
+        try:
+            sock.send(str(state).encode())
+        except:
+            print("Broken connection:", addr[0]+":"+str(addr[1]))
+            return
         while True:
             while self.lock == [1]:
                 pass
+            if self.stop_thread == [1]:
+                break
             current_state = self.games[:]
             if len(state) != len(current_state):
-                print("len1: "+str(len(state)))
-                print("len2: "+str(len(current_state)))
                 state = current_state[:]
-                sock.send(str(state).encode())
-                print("length different")
+                try:
+                    sock.send(str(state).encode())
+                except:
+                    print("Broken connection:", addr[0]+":"+str(addr[1]))
+                    return
                 continue
             # if length is unchanged, then previous state must contain at least one item which is not in current state ( in case that there is a difference )
+            # or something is changed for a particular game
             for game in state:
-                if game not in current_state:
-                    state = current_state[:]
-                    sock.send(str(state).encode())
-                    print("found difference")
+                found = False
+                break_loops = False
+                for master in current_state:
+                    if game["id"] == master["id"]:
+                        found = True
+                        keys = ['map', 'mods', 'name', 'address', 'players', 'state']
+                        for key in keys:
+                            if game[key] != master[key]:
+                                state = current_state[:]
+                                try:
+                                    print("found difference in one of games")
+                                    sock.send(str(state).encode())
+                                    break_loops = True
+                                    break
+                                except:
+                                    print("Broken connection:", addr[0]+":"+str(addr[1]))
+                                    return
+                        if break_loops:
+                            break
+                if break_loops:
                     break
+                if not found:
+                    state = current_state[:]
+                    try:
+                        print("game not found")
+                        sock.send(str(state).encode())
+                        break
+                    except:
+                        print("Broken connection:", addr[0]+":"+str(addr[1]))
+                        return
 
 if __name__ == "__main__":
+    network = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
+    network.bind(("",  33322))
+
     pinger = Pinger()
     ping_thread = threading.Thread(target=pinger.pinger)
+    clients = []    #a list of clients
     try:
-        ping_thread.start()
+        ping_thread.start() #start master server pinger
         
-        network = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
-        network.bind(("",  33321))
-        while 1:
+        while True:
             network.listen(1)
-            sock, addr = network.accept()
-            client = Client(pinger.data_json, pinger.lock)
+            sock, addr = network.accept()   #a new client connected
+            print("Connected:", addr[0]+":"+str(addr[1]))
+            client = Client(pinger.data_json, pinger.lock)  #share a list of games and lock state with client (lock is required when Pinger modifies list of games)
             client_thread = threading.Thread(target=client.work_with_client,  args=(sock, addr, ))
             client_thread.start()
-        network.close()
-    except:
+            clients.append(client)
+    except KeyboardInterrupt as e:
+        print("*** Stopping Threads... ***")
+        for cl in clients:
+            cl.stop_thread = [1]    #close works with clients
         pinger.stop_thread = [1]    #stop pinger thread
+        network.close()
         exit(1)
-        
+    network.close()
+    exit(0)
